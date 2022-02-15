29c5d753cc13d38dd7ab24efb6345edde91713bc
natabena <natabena@gmail.com>
2022-02-15 18:12:40 -0300

    se agregan librerias- mod footer header

---
+++ js/index.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', (eventLoad) => {
+   
+    document.querySelector("form-signin").addEventListener(
+        "submit", (eventoSubmit) => {
+        eventoSubmit.preventDefault();
+        
+
+        if(usuarioEsValido() && contrasenaEsValida() ){
+            validarUsuario();
+          }  else{
+                mostrarMensajeerror();
+          }
+    
+        return false
+
+    }) ;
+
+
+
+});
